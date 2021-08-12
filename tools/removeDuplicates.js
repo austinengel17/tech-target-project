0aab4cc4c07d8f33641971bbb4d5a4e7f52f2b0c
Austin <austinengel17@gmail.com>
2021-08-12 13:51:30 -0400

    add comments

--- tools/removeDuplicates.js
+++ tools/removeDuplicates.js
@@ -1,7 +1,10 @@
 module.exports = function removeDuplicates(itemArr) {
-    let jsonObject = itemArr.map(JSON.stringify);
 
+    //convert JSON to string (to make objects comparable) and map to new array
+    let jsonObject = itemArr.map(JSON.stringify);
+    //passes new array to set to remove duplicates
     let uniqueSet = new Set(jsonObject);
+    //converts set back to array and parses back to JSON
     let uniqueArray = Array.from(uniqueSet).map(JSON.parse);
 
     return uniqueArray;
